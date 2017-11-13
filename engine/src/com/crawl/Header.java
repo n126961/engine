0ac366b226f0cc3be03ea494ea0dd6c8bc8cce23
sandip <sandip.jobs@gmail.com>
2017-11-13 10:12:04 +0530

    all link extracts

--- engine/src/com/crawl/Header.java
+++ engine/src/com/crawl/Header.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
@@ -17,7 +18,7 @@ public class Header {
 	public void analyse(String url) throws Exception, IOException {
 		WebClient client = new WebClient(BrowserVersion.CHROME);
 		client.getOptions().setCssEnabled(false);
-		client.getOptions().setJavaScriptEnabled(true);
+		client.getOptions().setJavaScriptEnabled(false);
 		visitedLinks.add(url);
 		System.out.println(System.currentTimeMillis());
 		extracts(url, client);
@@ -31,10 +32,16 @@ public class Header {
 		List<HtmlElement> items = page.getByXPath("//div[@id='search-result-items']");
 		for (HtmlElement htmlItem : items) {
 			List<HtmlAnchor> allProducts = htmlItem.getByXPath(".//a");
-			for(HtmlAnchor product :allProducts){
-				System.out.println(product.asText());
+			for(HtmlAnchor productLink :allProducts){
+				System.out.println(productLink.asText());
+				HtmlPage returnPage=productLink.click();
+				returnPage.asXml();
+				System.out.println(returnPage.asXml());
+				System.out.println("returned");
 			}
 		}
+		
+		
 
 		
 	}
